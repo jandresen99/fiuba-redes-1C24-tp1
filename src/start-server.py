519b47015f6bc1e021bf98f28b575c88e48c5202
Joaquin Andresen <joaquin.andresen@Klar-MacBook-Pro.local>
2024-04-12 20:24:00 -0300

    Agrego gitignore

--- src/start-server.py
+++ src/start-server.py
@@ -1,5 +1,8 @@
 import socket
 import argparse
+import logging
+
+logger = logging.getLogger(__name__)
 
 def start():
     UDP_IP = "127.0.0.1"
@@ -7,24 +10,47 @@ def start():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind((UDP_IP, UDP_PORT))
-
-    print("Listening on {}:{}".format(UDP_IP, UDP_PORT))
+    logger.info("Listening on %s:%d", UDP_IP, UDP_PORT)
 
     while True:
         data, addr = sock.recvfrom(1024)
-        print("Message received:", data)
+        logger.info("Message received: %s", data)
 
-if __name__ == "__main__":
+def menu():
     parser = argparse.ArgumentParser(prog='start-server')
-    parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
-    parser.add_argument('-q', '--quiet', action='store_true', help='decrease output verbosity')
-    parser.add_argument('-H', '--host', default='localhost', help='service IP address')
-    parser.add_argument('-p', '--port', type=int, default=8080, help='service port')
-    parser.add_argument('-s', '--storage', required=True, help='storage dir path')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
+    group.add_argument('-q', '--quiet', action='store_true', help='decrease output verbosity')
+    parser.add_argument('-H', '--host', help='service IP address', metavar='ADDR')
+    parser.add_argument('-p', '--port', type=int, help='service port', metavar='PORT')
+    parser.add_argument('-s', '--storage', help='storage dir path', metavar='DIRPATH')
 
     args = parser.parse_args()
 
+    return args
+
+def main():
+    args = menu()
+
     if args.verbose:
-        print("verbosity activated")
+        level = logging.DEBUG
     elif args.quiet:
-        print("quiet activated")
+        level = logging.WARNING
+    else:
+        level = logging.INFO
+
+    logging.basicConfig(filename='server.log', level=level)
+    ch = logging.StreamHandler()
+    ch.setLevel(level)
+    logger.addHandler(ch)
+
+    start()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+    
+    
--- src/upload.py
+++ src/upload.py
@@ -3,7 +3,7 @@ import socket
 UDP_IP = "127.0.0.1"
 UDP_PORT = 6000
 
-MESSAGE = "Hola!"
+MESSAGE = b"Hola!"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
